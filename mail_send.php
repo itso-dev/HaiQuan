ac870d5f98c2b79dac2fefe142e487d5971a34a2
sooin <tkrhkthsu503@gmail.com>
2025-12-10 13:55:30 +0900

    메일 발송 수정

--- mail_send.php
+++ mail_send.php
@@ -6,14 +6,15 @@ if (isset($_POST['message'])) {
  $messageArr = json_decode($_POST['message'], true);
 }
 $message = mailForm($messageArr);
+$client_key = "itso";
 
-$email_sql = "select * from email_tbl where id = 1";
+$email_sql = "select * from email_tbl where client_key = '$client_key'";
 $email_stt=$db_conn->prepare($email_sql);
 $email_stt->execute();
 
 // 반복문으로 모든 이메일에 메일 발송
 while ($row = $email_stt->fetch(PDO::FETCH_ASSOC)) {
     $to_email = $row['email'];
-    mailer_google("랜딩페이지명", "dev@itso.co.kr", $to_email, "랜딩페이지명 랜딩 문의", $message, 1);
+    mailer_google("잇소", "dev@itso.co.kr", $to_email, "신규 랜딩 문의", $message, 1);
 }
 ?>
